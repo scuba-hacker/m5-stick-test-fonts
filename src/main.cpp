d1f888374cf11282841b0714c5d8d6414a437f18
Mark B. Jones aka The Scuba Hacker <131517566+scuba-hacker@users.noreply.github.com>
2025-10-04 14:45:10 +0100

    Added custom fonts demo

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,25 @@
 
 #include <TFT_eSPI.h>
 
+// Stock font and GFXFF reference handle
+#define GFXFF 1
+#define FF18 &FreeSans12pt7b
+
+// Custom are fonts added to library "TFT_eSPI\Fonts\Custom" folder
+// a #include must also be added to the "User_Custom_Fonts.h" file
+// in the "TFT_eSPI\User_Setups" folder. See example entries.
+#define CF_OL24 &Orbitron_Light_24
+#define CF_OL32 &Orbitron_Light_32
+#define CF_RT24 &Roboto_Thin_24
+#define CF_S24  &Satisfy_24
+#define CF_Y32  &Yellowtail_32
+
 void header(const char *string, uint16_t color);
 void drawDatumMarker(int x, int y);
+
+void loop_all_free_fonts_demo(); 
+void loop_free_font_demo();
+void loop_custom_fonts();
 
 TFT_eSPI& tft=M5.Lcd;
 
@@ -17,10 +34,119 @@ void setup()
   tft.setRotation(1);
 }
 
+void loop()
+{
+//  loop_all_free_fonts_demo(); 
+//  loop_free_font_demo();
+  loop_custom_fonts();
+}
 //  -D TFT_WIDTH=135
 //  -D TFT_HEIGHT=240
 
-void loop2() {
+void loop_custom_fonts() 
+{
+
+  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  // Show custom fonts
+  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+  // Where font sizes increase the screen is not cleared as the larger fonts overwrite
+  // the smaller one with the background colour.
+
+  // We can set the text datum to be Top, Middle, Bottom vertically and Left, Centre
+  // and Right horizontally. These are the text datums that can be used:
+  // TL_DATUM = Top left (default)
+  // TC_DATUM = Top centre
+  // TR_DATUM = Top right
+  // ML_DATUM = Middle left
+  // MC_DATUM = Middle centre <<< This is used below
+  // MR_DATUM = Middle right
+  // BL_DATUM = Bottom left
+  // BC_DATUM = Bottom centre
+  // BR_DATUM = Bottom right
+  // L_BASELINE = Left character baseline (Line the 'A' character would sit on)
+  // C_BASELINE = Centre character baseline
+  // R_BASELINE = Right character baseline
+
+  //Serial.println();
+
+  // Set text datum to middle centre (MC_DATUM)
+  tft.setTextDatum(MC_DATUM);
+
+  // Set text colour to white with black background
+  // Unlike the stock Adafruit_GFX library, the TFT_eSPI library DOES draw the background
+  // for the custom and Free Fonts
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+
+  tft.fillScreen(TFT_MAGENTA);            // Clear screen
+  tft.setFreeFont(FF18);                 // Select the font
+  tft.drawString("Yellowtail 32", TFT_HEIGHT/2,10, GFXFF);// Print the string name of the font
+  tft.setFreeFont(CF_Y32);                 // Select the font
+  tft.drawString(TEXT, TFT_HEIGHT/2,80, GFXFF);// Print the string name of the font
+  delay(2000);
+
+  tft.fillScreen(TFT_BLUE);            // Clear screen
+  tft.setFreeFont(FF18);                 // Select the font
+  tft.drawString("Satisfy 24", TFT_HEIGHT/2,10, GFXFF);// Print the string name of the font
+  tft.setFreeFont(CF_S24);                 // Select the font
+  tft.drawString(TEXT, TFT_HEIGHT/2,80, GFXFF);// Print the test text in the custom font
+  delay(2000);
+
+  tft.fillScreen(TFT_RED);            // Clear screen
+  tft.setFreeFont(FF18);                 // Select the font
+  tft.drawString("Roboto 24", TFT_HEIGHT/2,10, GFXFF);// Print the string name of the font
+  tft.setFreeFont(CF_RT24);                 // Select the font
+  tft.drawString(TEXT, TFT_HEIGHT/2,80, GFXFF);// Print the test text in the custom font
+  delay(2000);
+
+  tft.fillScreen(TFT_DARKGREY);            // Clear screen
+  tft.setFreeFont(FF18);                 // Select the font
+  tft.drawString("Orbitron 24", TFT_HEIGHT/2,10, GFXFF);// Print the string name of the font
+  tft.setFreeFont(CF_OL24);                 // Select the font
+  tft.drawString(TEXT, TFT_HEIGHT/2,80, GFXFF);// Print the test text in the custom font
+  delay(2000);
+  
+  // Here we do not clear the screen and rely on the new text over-writing the old
+  tft.setFreeFont(FF18);                 // Select the font
+  tft.drawString("Orbitron 32", TFT_HEIGHT/2,10, GFXFF);// Print the string name of the font
+  tft.setFreeFont(CF_OL32);                 // Select the font
+  tft.drawString(TEXT, TFT_HEIGHT/2,80, GFXFF);// Print the test text in the custom font
+  delay(2000);
+
+  // Here we use text background padding to over-write the old text
+  tft.fillScreen(TFT_YELLOW);            // Clear screen
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+
+  // Here we use text background padding to over-write the old text
+  tft.setTextPadding(tft.width() - 20); // Blanked area will be width of screen minus 20 pixels
+  tft.setFreeFont(FF18);                 // Select the font
+  tft.drawString("Orbitron 32 with padding", TFT_HEIGHT/2,10, GFXFF);// Print the string name of the font
+  tft.setFreeFont(CF_OL32);                 // Select the font
+  tft.drawString(TEXT, TFT_HEIGHT/2,80, GFXFF);// Print the test text in the custom font
+  delay(2000);
+
+  // Here we use text background padding to over-write the old text
+  tft.fillScreen(TFT_YELLOW);            // Clear screen
+  tft.setTextColor(TFT_BLUE, TFT_YELLOW);
+  tft.setTextPadding(tft.width() - 20); // Blanked area will be width of screen minus 20 pixels
+  tft.setFreeFont(FF18);                 // Select the font
+  tft.drawString("Orbitron 32 with padding", TFT_HEIGHT/2,10, GFXFF);// Print the string name of the font
+  // Use 80 pixel wide padding so old numbers over-write old ones
+  // One of the problrms with proportionally spaced numbers is that they jiggle position
+  tft.setTextPadding(80);
+  tft.setTextDatum(MC_DATUM);
+  tft.setFreeFont(CF_OL32);
+  for( int i = 100; i > 0; i--)
+  {
+    tft.drawNumber( i, TFT_HEIGHT/2,80);
+    delay(250);
+  }
+
+  // Reset text padding to zero (default)
+  tft.setTextPadding(0);
+}
+
+void loop_all_free_fonts_demo() {
 
   // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
   // Show all 48 fonts in centre of screen ( x,y coordinate 160,120)
@@ -338,7 +464,7 @@ void loop2() {
 }
 
 
-void loop() {
+void loop_free_font_demo() {
 
   int xpos =  0;
   int ypos = 40;
